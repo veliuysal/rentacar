82df0b037c1d0ffd580c6edd0277833d959c429d
veliuysal <veliuysaldev@gmail.com>
2023-12-31 11:00:45 +0300

    corrected brand save method

--- api/src/main/java/com/bilgeadam/rentacar/repository/BrandRepository.java
+++ api/src/main/java/com/bilgeadam/rentacar/repository/BrandRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
 
-    @Query(value = "SELECT NVL(MAX(b.id,0),0) FROM Brand b")
+    @Query(value = "SELECT COALESCE(MAX(b.id), 0) FROM Brand b")
     Integer getMaxId();
 }
